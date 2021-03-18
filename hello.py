7236e6a12251b43bb51a189694ebb39f7f379540
greezun <greezun@gmail.com>
2021-03-18 20:51:41 +0200

    правил приложение

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-def wsgi_application(environ, start_response):
-    status = '200 OK'
-    headers = [
-    ('Content-Type', 'text/plain')
-    ]
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    start_response(status, headers )
-    return  body
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
