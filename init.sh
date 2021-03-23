07eb42f866f00a79905b68571be3e4434f6c6ba6
greezun <greezun@gmail.com>
2021-03-23 19:47:31 +0200

    задача 2.1 (перша спроба)

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
