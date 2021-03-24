ca72240f03a85d10b3256d2e97a3f90cfb5d5d41
greezun <greezun@gmail.com>
2021-03-24 20:15:55 +0200

    задача 2.1 (третя спроба)

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,22 +1,14 @@
-"""ask URL Configuration
+from django.contrib import admin
+from django.urls import include, path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.test),
-]
+urlpatterns = {
+    path('qa/', include('qa.urls')),
+    path('admin/', admin.site.urls),
+    path('/', include('qa.urls')),
+    path('login/', include('qa.urls')),
+    path('signup/', include('qa.urls')),
+    path('question/\d+/$/ ', include('qa.urls')),
+    path('ask/', include('qa.urls')),
+    path('popular/', include('qa.urls')),
+    path('new/', include('qa.urls'))
+}
